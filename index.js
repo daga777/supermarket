bc701c436fc195e02c78262a891604a4035fbd06
daga777 <62566533+daga777@users.noreply.github.com>
2020-07-14 08:37:29 -0500

    july 13/20

---
+++ index.js
@@ -0,0 +1,113 @@
+const products = require("./products");
+const quantity = require("./quantity");
+const clients = require("./clients");
+const costs = require("./costs");
+
+/**
+ * Puedes crear las variables y funciones necesarias para que el sistema funcione.
+ * Debe simular una compra en una tienda y arrojar el total de la compra teniendo en cuenta los siguientes descuentos:
+ * León Ceballos: 5%
+ * Cristian Vega: 10%
+ * Jesse Cogollo: 2%
+ **/
+
+const productsListIds = [34, 1, 9, 8, 23, 11, 40, 14, 6, 33, 29, 20, 41, 15, 5];
+const productsQuantities = [
+  2,
+  5,
+  3,
+  9,
+  2,
+  4,
+  2,
+  3,
+  10,
+  8,
+  6,
+  12,
+  4,
+  5,
+  2,
+  7,
+  7,
+];
+
+/**
+ * Requerimientos:
+ * 1: Dentro de la función principal deberás imprimir la lista de productos con la cantidad restante después de la compra.
+ * 2: Imprimir el valor total sin aplicar el descuento. El número debe tener dos decimales.
+ * 3: Imprimir el total del descuento aplicado.
+ * 4: Si no hay suficientes productos se debe agregar a la lista de compra solo los que alcancen.
+ **/
+
+// TODO: Si crees que es necesesario incluye más funciones para las pequeñas tareas
+
+// Funciones adicionales aquí...
+
+// TODO: Función Principal getCostList
+function getCostList(client, productsIds, quantities) {
+  // Código aquí...
+  //const productValues = {
+  //  name:products[productsIds[0]],
+  //  value:costs[productsIds[0]],
+  //  qnty:quantity[productsIds[0]]
+  //};
+
+  const clientlist = [];
+  const storeStock = [];
+
+  productsIds.map(function (productToBuy, index) {
+    const productValues = {
+      name: products[productToBuy],
+      value: costs[productToBuy],
+      qnty: quantity[productToBuy],
+      qntyToBuy: quantities[index],
+    };
+    //    storeStock;
+
+    if (productValues.qnty >= productValues.qntyToBuy) {
+      const newStock = productValues.qnty - productValues.qntyToBuy;
+      //    productsIds(34) quantity=newStock(index)
+      quantity[productToBuy] = newStock;
+    } else {
+      quantity[productToBuy] = 0;
+    }
+    console.log(products[productToBuy], quantity[productToBuy]);
+    clientlist.push(productValues);
+  });
+
+  productsIds.map(function (productToBuy, index) {
+    const totalcost = costs[productToBuy] * quantity[productToBuy];
+    console.log("Total cost", totalcost);
+  });
+
+  // products[productsToBuy] ,   costs[productsToBuy] *  quantity[productsToBuy]
+
+  //  console.log(clientlist);
+  //  console.log(storeStock);
+}
+
+const result = getCostList(clients[0], productsListIds, productsQuantities);
+
+// Imprime el total de la compra
+console.log("El total de la compra es: " + result);
+
+/**
+ * TE RECOMENDAMOS
+ * 1. Primero recorre, suma y multiplica los productos de acuerdo a la cantidad
+ * 2. Saca el descuento según el cliente
+ * 3. Aplica el descuento  al total
+ * 4. Al utilizar el console.log pon titulos a lo que imprimes
+ */
+
+/**
+ * LISTA DE CHEQUEO
+ * Calcular el total de la compra
+ * Imprimir los productos con la  cantidad restante luego de la compra
+ * Calcular el descuento
+ * Imprimir el descuento
+ * Aplicar descuento al total
+ * Imprime el total
+ */
+
+// purchase
